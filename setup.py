081ed4a38ca591014f5bca8ace0c35600dae0903
Cece Hedrick <cecehedrick@gmail.com>
2017-11-14 17:25:11 -0700

    attempts to make project installable via pip issue #5

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="ipython-IDV",
+    version="0.1.0",
+    url="https://github.com/piqueen314/ipython-IDV.git",
+
+    author="Cece Hedrick",
+    author_email="cecehedrick@gmail.com",
+
+    description="This project allows users to use Unidata's IDV with jupyter notebooks ",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
