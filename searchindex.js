231cfc88c27874f453d6f107a43b1511aae0986a
Cece Hedrick <cecehedrick@gmail.com>
2017-12-11 15:34:45 -0700

    publish the docs

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to ipython-IDV\u2019s documentation!"],titleterms:{document:0,idv:0,indic:0,ipython:0,tabl:0,welcom:0}})
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to CECETEST ipython-IDV\u2019s documentation!"],titleterms:{cecetest:0,document:0,idv:0,indic:0,ipython:0,tabl:0,welcom:0}})
